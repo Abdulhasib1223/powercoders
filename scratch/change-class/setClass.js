732336ccf9eaa4b6b2ece3f630c845e59fa9e3be
Abdul Azzam <abdulhasib.azzam@powercoders.org>
2018-12-14 14:32:06 +0100

    Refactoring:Repeated code

---
+++ scratch/change-class/setClass.js
@@ -0,0 +1,27 @@
+console.log('My code is running!');
+
+function greet(name) {
+  console.log('Hello, ' + name);
+}
+
+function setClassTip(event) {
+  let el = document.getElementById('mypara');
+  el.className = 'tip';
+}
+
+function setClassWarning(event) {
+  let el = document.getElementById('mypara');
+  el.className = 'warning';
+}
+
+function clearClass(event) {
+  let el = document.getElementById('mypara');
+  el.className = '';
+}
+
+  //function listenForClick(event) {
+  //Do something
+
+ //<p onclick="listenForClick(event);">Some text</p>
+
+ // javascript code goes here (this is a comment)
