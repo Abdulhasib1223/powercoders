ae80a3f295d05431cf4fe48aa7a79c0bb52fa6e5
Abdul Azzam <abdulhasib.azzam@powercoders.org>
2018-12-20 13:58:49 +0100

    JS examples

---
+++ scratch/hide_show_ingredients/recipe.js
@@ -0,0 +1,20 @@
+function setClass(event,className) {
+    let el = document.getElementById('mypara');
+    el.className = className;
+}
+
+document.addEventListener('DOMContentLoaded', function (event){
+    let makeWarning = document.getElementById('make-warning');
+    makeWarning.addEventListener('click', function (event){
+        setClass(event,'warning');
+    });
+
+    let makeTip = document.getElementById('make-tip');
+    makeTip.addEventListener('click', function (event) {
+        setClass(event,'tip');
+    });
+    let normal = document.getElementById('normal');
+    normal.addEventListener('click', function (event) {
+        setClass(event,'');
+    });
+});jhgjhgjhg
