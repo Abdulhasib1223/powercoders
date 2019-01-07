60a2f5649e0112121f7c167ccc3793a558f16a00
Abdul Azzam <abdulhasib.azzam@powercoders.org>
2019-01-07 17:42:29 +0100

    applying some tricks on shopping list :)

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -4,3 +4,18 @@ document.addEventListener('DOMContentLoaded',function (event) {
        console.log(inputBox.value);
    });
 });
+
+console.log('my list');
+
+function list(itemName) {
+    let elli1 = document.createElement('li');
+    let elspan = document.createElement('span');
+    elspan.innerText = itemName;
+
+    elli1.appendChild(elspan);
+    let elbutton = document.createElement('button')
+    elbutton.innerText ='Delete';
+    elli1.appendChild(elbutton);
+    return elli1;
+
+}
