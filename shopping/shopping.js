7d1ec85604a5119f13178a23f0f55096c157f24e
Abdul Azzam <abdulhasib.azzam@powercoders.org>
2019-01-08 16:51:37 +0100

    applying some tricks on shopping list adding and removing button

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,21 +1,28 @@
-document.addEventListener('DOMContentLoaded',function (event) {
-   document.querySelector('button').addEventListener('click',function (event) {
-       let inputBox = document.getElementById('item');
-       console.log(inputBox.value);
-   });
+document.addEventListener('DOMContentLoaded', function (event) {
+  document.querySelector('button').addEventListener('click', function (event) {
+    document.querySelector('ul').appendChild(list(document.getElementById('item').value));
+
+    let inputBox = document.getElementById('item');
+    console.log(inputBox.value);
+  });
 });
 
 console.log('my list');
 
 function list(itemName) {
-    let elli1 = document.createElement('li');
-    let elspan = document.createElement('span');
-    elspan.innerText = itemName;
+  let elli1 = document.createElement('li');
 
-    elli1.appendChild(elspan);
-    let elbutton = document.createElement('button')
-    elbutton.innerText ='Delete';
-    elli1.appendChild(elbutton);
-    return elli1;
+  let elspan = document.createElement('span');
+  elspan.innerText = itemName;
+  elli1.appendChild(elspan);
 
+  let elbutton = document.createElement('button');
+  elbutton.innerText = 'Delete';
+
+  elbutton.addEventListener('click', function (event) {
+    console.log('Delete button is clicked');
+    elli1.remove();
+  });
+  elli1.appendChild(elbutton);
+  return elli1;
 }
