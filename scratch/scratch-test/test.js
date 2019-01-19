e5c3251889e3bbd89d3ee3bc0242a875d330f4cf
Abdul Azzam <abdulhasib.azzam@powercoders.org>
2019-01-19 17:47:31 +0100

    addaing paragraph using js

--- scratch/scratch-test/test.js
+++ scratch/scratch-test/test.js
@@ -8,8 +8,25 @@ function creating() {
     elBody.appendChild(elP);
 }
 
-document.addEventListener('DOMContentLoaded'/*That is the name of the event that is triggered after the DOM is loaded*/, function (event) /*function(event) is an anonymous function used as an event listener and the event is (DOMloadedContent in our case)*/ {
+document.addEventListener('DOMContentLoaded'  /*That is the name of the event that is triggered after the DOM is loaded*/, function (event) /*function(event) is an anonymous function used as an event listener and the event is (DOMloadedContent in our case)*/ {
     document.getElementById('creating').addEventListener('click' /*That is the name of the event that is triggered after the element containing the addpara id is clicked*/, function (event) {
         creating();
     });
 });
+
+function elseTest(num) {
+  if (num < 10) {
+    console.log('less then 10');
+  } else if (num > 10) {
+    console.log('bigger then 10');
+  } else if (num === 10);
+  console.log('exactly 10')
+}
+
+//another easier way to write it:
+
+function elseTest6(num) {
+  if (trimmedValue === '') {
+    return;
+  }
+}
