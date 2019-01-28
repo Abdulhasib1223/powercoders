7ecc106cfd998fd7de4ee1629ddaf02c6b75d6e6
Abdul Azzam <abdulhasib.azzam@powercoders.org>
2019-01-28 10:57:31 +0100

    removing variables and doing some chaining

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,28 +1,80 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-  document.querySelector('button').addEventListener('click', function (event) {
-    let inputBox = document.getElementById('item');
-    document.querySelector('ul').appendChild(
-        createNewListItem(inputBox.value));
+  /** Widget that the user types on item in to. */
+  const inputBox = document.getElementById('item');
+  const shoppingList = document.querySelector('ul');
+  const addItemButton = document.querySelector('button');
+  const btn = document.getElementById('clear');
+  btn.addEventListener('click', function (event) {
+    let arr = document.querySelectorAll('li');
+    arr.forEach(function (el) {
+      el.remove();
+      const clearListButton = document.querySelector('button');
+      const listItem = document.querySelectorAll('li');
+      clearListButton.disabled = listItem.length === 0;
+      // or we can chain it like this:
+      // document.querySelector('button#clear').disabled =
+      //   document.querySelectorAll('li').length === 0;
+
+
+    });
+    btn.disabled = true;
   });
 
-  document.querySelector('input').addEventListener('keyup', function (event) {
-    if (event.key === 'Enter') {
-      let inputBox = document.getElementById('item');
-      document.querySelector('ul').appendChild(
-          createNewListItem(inputBox.value));
+  inputBox.addEventListener('keyup', function (event) {
+    const trimmedValue = inputBox.value.trim();
+    addItemButton.disabled = trimmedValue === '';
+
+    if (trimmedValue === '') {
+      return;
     }
+
+    if (event.key !== 'Enter') {
+      return;
+    }
+
+    shoppingList.appendChild(createNewListItem(trimmedValue));
+    inputBox.value = '';
+    addItemButton.disabled = true;
+    btn.disabled = false;
+
   });
+
+  addItemButton.addEventListener('click', function (event) {
+    const trimmedValue = inputBox.value.trim();
+
+    shoppingList.appendChild(createNewListItem(inputBox.value.trim()));
+    inputBox.value = '';
+    inputBox.focus();
+    addItemButton.disabled = true;
+    btn.disabled = false;
+  });
+
+  inputBox.focus();
+  addItemButton.disabled = true;
+  btn.disabled = true;
+
 });
 
-function createNewListItem(itemName) {
-  let li = document.createElement('li');
+/**
+ * Creates and returns an 'li' elements for inclusion in the shopping list.
+ * The li element that is returned has the structure
+ * '&lt;li><span>itemName</span> <button>Delete</button></li>
+ * read the code to understand the DOM tree structure.
+ *
+ * @param {string} itemName name of the item to add to the list
+ * @returns {HTMLElement} li element
+ */
 
-  let elSpan = document.createElement('span');
+
+function createNewListItem(itemName) {
+  const li = document.createElement('li');
+
+  const elSpan = document.createElement('span');
   elSpan.innerText = itemName;
   li.appendChild(elSpan);
 
-  let elButton = document.createElement('button');
-  elButton.innerText = 'Delete';
+  const elButton = document.createElement('button');
+  elButton.innerText = '  Delete';
 
   elButton.addEventListener('click', function (event) {
     console.log('Delete button is clicked');
@@ -31,3 +83,10 @@ function createNewListItem(itemName) {
   li.appendChild(elButton);
   return li;
 }
+
+
+//  listitems.length;
+//  inputBox.focus();
+//  addItembutton.disabled = true;
+//  clearListButton.disabled = true;
+
