5a0eec0c69949f598de09c18948c8b7eb19f7fab
Abdul Azzam <abdulhasib.azzam@powercoders.org>
2019-01-29 16:44:18 +0100

    adding a quantity box to make the app more practical

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,10 +1,12 @@
 function domContentLoaded() {
   /** Widget that the user types on item in to. */
   const inputBox = document.getElementById('item');
+  const quantityBox = document.getElementById('quantity');
   const shoppingList = document.querySelector('ul');
   const addItemButton = document.querySelector('button');
   const btn = document.getElementById('clear');
-  btn.addEventListener('click', function (event) {
+
+  btn.addEventListener('click', function () {
     let arr = document.querySelectorAll('li');
     arr.forEach(function (el) {
       el.remove();
@@ -13,9 +15,7 @@ function domContentLoaded() {
       clearListButton.disabled = listItem.length === 0;
       // or we can chain it like this:
       // document.querySelector('button#clear').disabled =
-      //   document.querySelectorAll('li').length === 0;
-
-
+      // document.querySelectorAll('li').length === 0;
     });
     btn.disabled = true;
   });
@@ -32,17 +32,16 @@ function domContentLoaded() {
       return;
     }
 
-    shoppingList.appendChild(createNewListItem(trimmedValue));
+    shoppingList.appendChild(createNewListItem(trimmedValue, quantityBox.value));
     inputBox.value = '';
     addItemButton.disabled = true;
     btn.disabled = false;
-
   });
 
-  addItemButton.addEventListener('click', function (event) {
+  addItemButton.addEventListener('click', function () {
     const trimmedValue = inputBox.value.trim();
 
-    shoppingList.appendChild(createNewListItem(trimmedValue));
+    shoppingList.appendChild(createNewListItem(trimmedValue, quantityBox.value.trim()));
     inputBox.value = '';
     inputBox.focus();
     addItemButton.disabled = true;
@@ -52,7 +51,6 @@ function domContentLoaded() {
   inputBox.focus();
   addItemButton.disabled = true;
   btn.disabled = true;
-
 }
 
 if (document.readyState === 'loading') {
@@ -64,7 +62,6 @@ if (document.readyState === 'loading') {
   domContentLoaded();
 }
 
-
 /**
  * Creates and returns an 'li' elements for inclusion in the shopping list.
  * The li element that is returned has the structure
@@ -72,29 +69,34 @@ if (document.readyState === 'loading') {
  * read the code to understand the DOM tree structure.
  *
  * @param {string} itemName name of the item to add to the list
+ * @param {string} quantity Quantity of the item to append to the list
  * @returns {HTMLElement} li element
  */
-function createNewListItem(itemName) {
-  const li = document.createElement('li');
 
+function createNewListItem(itemName, quantity) {
+  const listItem = document.createElement('li');
   const elSpan = document.createElement('span');
   elSpan.innerText = itemName;
-  li.appendChild(elSpan);
+  listItem.appendChild(elSpan);
+
+  if (quantity !== '') {
+    listItem.appendChild(document.createTextNode(' '));
+    const quantityText = document.createElement('span');
+    quantityText.textContent = `(${quantity})`;
+    listItem.appendChild(quantityText);
+  }
 
   const elButton = document.createElement('button');
   elButton.innerText = '  Delete';
-
-  elButton.addEventListener('click', function (event) {
+  elButton.addEventListener('click', function () {
     console.log('Delete button is clicked');
-    li.remove();
+    listItem.remove();
   });
-  li.appendChild(elButton);
-  return li;
+  listItem.appendChild(elButton);
+  return listItem;
 }
 
-
-//  listitems.length;
+//  listItems.length;
 //  inputBox.focus();
-//  addItembutton.disabled = true;
+//  addItemButton.disabled = true;
 //  clearListButton.disabled = true;
-
