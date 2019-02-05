9e302a0ee2555acb4f701b4f996d3fcc158e8f31
Abdul Azzam <abdulhasib.azzam@powercoders.org>
2019-02-05 15:56:55 +0100

    deleting dummy data and activating the button after adding list item

--- shopping-mvc/src/js/model.js
+++ shopping-mvc/src/js/model.js
@@ -9,11 +9,7 @@ class Model {
     console.log('Model initialised');
 
     /** @private {!ShoppingListItem[]} Items in the list */
-    this.items_ = [
-        new ShoppingListItem('First item', 'q1'),
-        new ShoppingListItem('Second item', 'q2'),
-        new ShoppingListItem('third item', 'q3')
-    ];
+    this.items_ = [];
 
     /** @private {!View} View gor this model. */
     this.view_ = new View(this, controller);
@@ -22,5 +18,14 @@ class Model {
   }
   get items () {
     return this.items_.slice();
+  }
+
+  /**
+   * Appends a new item to the list.
+   * @param item {!ShoppingListItem}
+   */
+  append(item) {
+    this.items_.push(item);
+    this.view_.update();
   }
 }
